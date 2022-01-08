d0ff88e44db63e54b2c57bf7a9a2bd0e623671fa
zhouhanging <3493329756@qq.com>
2022-01-08 13:39:37 +0800

    bebopopo

--- src/App.vue
+++ src/App.vue
@@ -34,19 +34,19 @@ export default {
     }
     
     //页面钩子函数:页面加载完成后触发
-    setTimeout(() => {                             //定时器,created执行一秒后触发
+    setTimeout(() => {             //定时器,created执行一秒后触发
       window.L2Dwidget.init({
-        pluginRootPath: 'live2dw/',                 //指向你的目录
-        pluginJsPath: 'lib/',                       //指向你的目录
-        pluginModelPath: 'live2d-widget-model-nico/assets/',                                   //中间这个koharu就是你的老婆,想换个老婆,换这个就可以了
+        pluginRootPath: 'live2dw/',                 
+        pluginJsPath: 'lib/',                       
+        pluginModelPath: 'live2d-widget-model-nico/assets/',                                 
         tagMode: false,
         debug: false,
-        model: { jsonPath: '/live2dw/live2d-widget-model-koharu/assets/koharu.model.json' },       //中间这个koharu就是你的老婆,想换个老婆,换这个就可以了
-        display: { position: 'right', width: 150, height: 300 },                               //调整大小,和位置
-        mobile: { show: true },                                                                //要不要盯着你的鼠标看
+        model: { jsonPath: '/live2dw/live2d-widget-model-koharu/assets/koharu.model.json' },      
+        display: { position: 'right', width: 150, height: 300 },                             
+        mobile: { show: true },                                                      
         log: false  
       })
-    }, 300)                                         //页面加载完成之后多久触发(好像是这个)
+    }, 300)                                      
   }
   
 };
